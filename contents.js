1a448055ed621a7b68aa1020a7d80a088c4038b6
Vecvo <vecvo@outlook.com>
2017-04-30 13:49:45 +0800

    Update contents.js

--- contents.js
+++ contents.js
@@ -182,11 +182,11 @@ function displayToc(filter) {
 // this function creates a three-column table and adds it to the screen
    var numDisplayed = 0;
    var tocTable = '';
-   var tocHead1 = 'JUDUL ARTIKEL';
+   var tocHead1 = 'TITLE';
    var tocTool1 = 'Click to sort by title';
    var tocHead2 = 'DATE';
    var tocTool2 = 'Click to sort by date';
-   var tocHead3 = 'KATEGORI';
+   var tocHead3 = 'CATEGORY';
    var tocTool3 = '';
    if (sortBy == "titleasc") { 
       tocTool1 += ' (descending)';
